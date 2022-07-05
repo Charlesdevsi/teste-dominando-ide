093ba1728ef6527890084398bcc8e6f417593c33
Charles Souza <charlessi779@gmail.com>
2022-07-04 21:27:55 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Cachorro caozinho = new Cachorro();
+		
+		System.out.println(gato);
+		System.out.println(caozinho);
+		
+		
+		
+		/*int a = 5;
+		int b = 15;
+		System.out.println("eu quero comer sua piriquita" + (a+b));*/
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Cachorro {
+	private String nome;
+	private String nmetidas;
+}
+
